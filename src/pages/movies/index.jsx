57bb680e1530a06ad55123312794ad39e18684b7
Diego Fagundes <fagundesdiego2015bolcombr@gmail.com>
2024-01-27 20:43:18 -0300

    fix: correção dos nomes de categoria na rota filmes

--- src/pages/movies/index.jsx
+++ src/pages/movies/index.jsx
@@ -1,43 +1,40 @@
-import { useEffect } from 'react'
-import {useState} from 'react'
-import { getMovieUpcoming, getTopMovies, getMovieLatest } from '../../services/getData'
-import Slider from '../../components/Slider'
+import { useEffect } from "react";
+import { useState } from "react";
+import {
+  getMovieUpcoming,
+  getTopMovies,
+  getMovieLatest,
+} from "../../services/getData";
+import Slider from "../../components/Slider";
 
 function Movies() {
-    const [topMovies, setTopMovies] = useState()
-    const [moviesLatest, setMoviesLatest] = useState()
-    const [moviesUpcoming, setMoviesUpcoming] = useState()
-    console.log(moviesLatest);
+  const [topMovies, setTopMovies] = useState();
+  const [moviesLatest, setMoviesLatest] = useState();
+  const [moviesUpcoming, setMoviesUpcoming] = useState();
+  console.log(moviesLatest);
 
-    useEffect(() => {
-      async function getAllData() {
-        Promise.all([
-            getTopMovies(),
-            getMovieLatest(),
-            getMovieUpcoming()
-        ]).then(
-            ([
-                topMovies,
-                moviesLatest,
-                moviesUpcoming
-            ]) => {
-                setTopMovies(topMovies),
-                setMoviesLatest(moviesLatest),
-                setMoviesUpcoming(moviesUpcoming)
-            }
-        )
-      }
-      getAllData()
-    }, [])
-    
-    
+  useEffect(() => {
+    async function getAllData() {
+      Promise.all([getTopMovies(), getMovieLatest(), getMovieUpcoming()]).then(
+        ([topMovies, moviesLatest, moviesUpcoming]) => {
+          setTopMovies(topMovies),
+            setMoviesLatest(moviesLatest),
+            setMoviesUpcoming(moviesUpcoming);
+        }
+      );
+    }
+    getAllData();
+  }, []);
+
   return (
-    <div style={{marginTop: '100px'}}>
-        {topMovies && <Slider info={topMovies} title={'Top Séries'}/>}
-        {moviesLatest && <Slider info={moviesLatest} title={'Filmes nos Cinemas' }/>}
-        {moviesUpcoming && <Slider info={moviesUpcoming} title={'Proximos Filmes'}/>}
+    <div style={{ marginTop: "100px" }}>
+      {topMovies && <Slider info={topMovies} title={"Top Filmes"} />}
+      {moviesLatest && <Slider info={moviesLatest} title={"Filmes Recentes"} />}
+      {moviesUpcoming && (
+        <Slider info={moviesUpcoming} title={"Proximos Filmes"} />
+      )}
     </div>
-  )
+  );
 }
 
-export default Movies
+export default Movies;
--- src/services/api.js
+++ src/services/api.js
@@ -5,7 +5,7 @@ const api = axios.create({
   params: {
     api_key: "83f9bc68df195b61c06b93f3ab88a5ca",
     language: "pt-BR",
-    page: 1,
+    page: 3,
   },
 });
 
